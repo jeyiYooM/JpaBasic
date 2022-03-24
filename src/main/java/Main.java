f064a9677de0e1021c18511e62bc255c374984b9
jeyiYooM <dbwpdl7899@gmail.com>
2022-03-24 13:08:15 +0900

    Jpa Basic Study Start

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import javax.persistence.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+
+    }
+}
